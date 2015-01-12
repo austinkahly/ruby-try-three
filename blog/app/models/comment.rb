c5aa46b01623bd77140b1851e1c4449af9aa219f
Austin Kahly <austin@wantable.com>
2015-01-12 08:40:45 -0600

    Updating from Pull Request
    Deleting a lot of files to clean up code and fixing indentation, mainly.

--- blog/app/models/comment.rb
+++ blog/app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
+  validates :commenter, presence: true, length: { minimum: 5 } 
+  validates :body, presence: true, length: { minimum: 4}
 end
